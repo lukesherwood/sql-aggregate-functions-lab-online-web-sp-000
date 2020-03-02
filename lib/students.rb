3f080498a86b6755034663542de1579b26bf71bd
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-02 23:00:53 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,8 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  ""
+  #SELECT column_name FROM table_name ORDER BY column_name ASC|DESC;
+  "SELECT "
 end
 
 def lowest_student_gpa
